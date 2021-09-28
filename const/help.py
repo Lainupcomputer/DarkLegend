b8b55b6a5b0765e5dd99a2255f64cca46ec2a0f7
lain <sandrokalett@gmail.com>
2021-09-28 16:34:55 +0200

    named pages

--- const/help.py
+++ const/help.py
@@ -1,21 +1,24 @@
 import discord
+import io
+
+prefix = io.get("Bot", "prefix")
 
 buttons = ["🔎", "🎲", "💰", "🎶", "⚙", "🗑"]
 
-page1 = discord.Embed(title="🔎", description="page1:\n"
+page1 = discord.Embed(title="🔎", description="Help:\n"
                                               "**Hier wird grade Gearbeitet **", colour=discord.Colour(0xb8e986))
 
-page2 = discord.Embed(title="🎲", description="page2:\n"
+page2 = discord.Embed(title="🎲", description="Fun:\n"
                                               "**Hier wird grade Gearbeitet **\n"
                                               "**Hier wird grade Gearbeitet **\n", colour=discord.Colour(0xb8e986))
 
-page3 = discord.Embed(title="💰", description="page3:\n"
+page3 = discord.Embed(title="💰", description="General:\n"
                                               "**Hier wird grade Gearbeitet **", colour=discord.Colour(0xb8e986))
 
-page4 = discord.Embed(title="🎶", description="page4:\n"
+page4 = discord.Embed(title="🎶", description="Musikbot:\n"
                                               "**Hier wird grade Gearbeitet **", colour=discord.Colour(0xb8e986))
 
-page5 = discord.Embed(title="⚙", description="page5:\n"
+page5 = discord.Embed(title="⚙", description="Admin Tools:\n"
                                               "**Hier wird grade Gearbeitet **", colour=discord.Colour(0xb8e986))
 
 
